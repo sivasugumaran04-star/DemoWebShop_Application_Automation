42e5bfaf5803eeff6f4a57f5159d3a79ad93e011
sivasugumaran04-star <sivasugumaran04@gmail.com>
2026-05-12 11:45:08 +0530

    Add files via upload

---
+++ Src /test/Java/Checkout.java
@@ -0,0 +1,14 @@
+package tests;
+
+import org.testng.annotations.Test;
+
+import utilities.BaseClass;
+
+public class Checkout extends BaseClass {
+
+	@Test
+	public void Checkout() {
+		Checkout cp= new Checkout();
+		cp.Checkout();
+	}
+}
